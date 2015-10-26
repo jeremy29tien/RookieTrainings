d8e4f50bee3603e2dc4533e08e8d9b3aca89527e
Heather Baker <heather@heatherbaker.com>
2015-10-25 22:15:37 -0700

    Created instructions
    Created instructions for MakeMath project which covers topics from
    Training 4

--- src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-
-public class HelloWorld {
-	
-	public static void main (String [] args ) {
-		// Welcome to Java training for the fourth week
-	}
-
-}
---
+++ src/MakeMath.java
@@ -0,0 +1,54 @@
+
+public class MakeMath {
+	public static void main(String []args) {
+		// create an array with 4 indexes
+		
+		// create a for loop that while i is less than the 
+		// yourArray.length, then it will set the element to i at 
+		// the index of yourArray that is equal to i
+		// During this, print your array
+		
+		System.out.println("onto next");
+		
+		// for each in yourArray, add 1
+		// During this, print yourArray
+		
+		System.out.println("onto next");
+		
+		// create an integer x with value 0
+		// while x is less than yourArray, add one to x
+		// during this, print x
+		
+		System.out.println("onto next");
+		
+		// for each element in yourArray
+		// if each is less than 0 print blah
+		// else if each is less than 1 print bleh
+		// else if each is less than 2 print blooh
+		// else print raaaaawr
+
+		System.out.println("Done!");
+		
+		// after running, it should look something like:
+		// 0
+		// 1
+		// 2
+		// 3
+		// onto next
+		// 1
+		// 2
+		// 3
+		// 4
+		// onto next
+		// 0
+		// 1
+		// 2
+		// 3
+		// onto next
+		// bleh
+		// blooh
+		// raaaaawr
+		// raaaaawr
+		// Done!
+	}
+}
